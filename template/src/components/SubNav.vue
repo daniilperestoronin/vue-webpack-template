ad4c229d82902f791e27dcf4bae8c25d10f00b4b
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-04-17 11:16:45 +0300

    [Add] SubNav

---
+++ template/src/components/SubNav.vue
@@ -0,0 +1,17 @@
+<template>
+   <b-nav>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+export default {
+  name: 'SubNav'
+};
+</script>
+
+<style>
+</style>
