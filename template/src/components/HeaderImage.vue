dbd3712e59bf8d98f1d5e6cfca23cfc603f3674c
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-04-17 11:24:39 +0300

    [Add] HeaderImage

---
+++ template/src/components/HeaderImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-img src="https://images.pexels.com/photos/317385/pexels-photo-317385.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" fluid-grow alt="Fluid-Grow image" />
+</template>
+
+<script>
+export default {
+  name: 'HeaderImage'
+};
+</script>
+
+<style>
+
+</style>
