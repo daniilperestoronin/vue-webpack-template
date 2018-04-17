d714640d2f49446881408ba721368d043afd6150
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-04-17 11:41:22 +0300

    [Add] MediaContainer

---
+++ template/src/components/MediaContainer.vue
@@ -0,0 +1,40 @@
+<template>
+    <b-container>
+    <ul class="list-unstyled">
+  <b-media tag="li">
+    <b-img slot="aside" blank blank-color="#abc" width="64" alt="placeholder" />
+    <h5 class="mt-0 mb-1">List-based media object</h5>
+    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+    ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus
+    viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec
+    lacinia congue felis in faucibus.
+  </b-media>
+  <b-media tag="li" class="my-4">
+    <b-img slot="aside" blank blank-color="#cba" width="64" alt="placeholder" />
+    <h5 class="mt-0 mb-1">List-based media object</h5>
+    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+    ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus
+    viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec
+    lacinia congue felis in faucibus.
+  </b-media>
+  <b-media tag="li">
+    <b-img slot="aside" blank blank-color="#bac" width="64" alt="placeholder" />
+    <h5 class="mt-0 mb-1">List-based media object</h5>
+    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+    ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus
+    viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec
+    lacinia congue felis in faucibus.
+  </b-media>
+  </ul>
+      </b-container>
+</template>
+
+<script>
+export default {
+    name: 'MediaContainer'
+};
+</script>
+
+<style>
+
+</style>
