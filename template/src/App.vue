d35bc7ad02feed9a059ab204af0c98781db9e52f
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-04-20 23:28:31 +0300

    Add footer

--- template/src/App.vue
+++ template/src/App.vue
@@ -5,6 +5,7 @@
     <header-image/>
     <card-container/>
     <media-container/>
+    <app-footer/>
   </div>
 </template>
 
@@ -14,6 +15,7 @@ import SubNav from "./components/SubNav.vue";
 import HeaderImage from "./components/HeaderImage.vue";
 import CardContainer from "./components/CardContainer.vue";
 import MediaContainer from "./components/MediaContainer.vue";
+import AppFooter from "./components/AppFooter.vue";
 export default {
   name: "app",
   components: {
@@ -21,7 +23,8 @@ export default {
     SubNav,
     HeaderImage,
     CardContainer,
-    MediaContainer
+    MediaContainer,
+    AppFooter
   }
 };
 </script>
---
+++ template/src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+<b-container fluid style="height:50px" class="text-light text-center bg-info">
+    <b-row align-v="end" class="text-center">
+        <b-col>About</b-col>
+        <b-col>vue-webpack-template<br> 2018</b-col>
+        <b-col>Contacts</b-col>
+    </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<style>
+
+</style>
