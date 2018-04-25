415255c6073d775fef2bae8c71f671baaa53b09a
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-04-25 23:58:32 +0300

    Add vue-goodshare

--- template/src/components/AppFooter.vue
+++ template/src/components/AppFooter.vue
@@ -2,15 +2,22 @@
 <b-container fluid style="height:50px" class="text-light text-center bg-info">
     <b-row align-v="start" class="text-center">
         <b-col>About</b-col>
-        <b-col>vue-webpack-template<br> 2018</b-col>
-        <b-col>Contacts</b-col>
+        <b-col>vue-webpack-template<br>© Copyright 2018 Daniil Perestoronin</b-col>
+        <b-col>Contacts
+<vue-goodshare></vue-goodshare>
+        </b-col>
     </b-row>
 </b-container>
 </template>
 
 <script>
+import VueGoodshare from 'vue-goodshare'
+
 export default {
-    name: 'AppFooter'
+    name: 'AppFooter',
+    components: {
+        VueGoodshare
+  }
 }
 </script>
 
