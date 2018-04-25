f714516ece06bcd233bad26add169aac7609e5ea
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-04-25 17:54:40 +0300

    Change footer

--- template/src/components/AppFooter.vue
+++ template/src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
 <b-container fluid style="height:50px" class="text-light text-center bg-info">
-    <b-row align-v="end" class="text-center">
+    <b-row align-v="start" class="text-center">
         <b-col>About</b-col>
         <b-col>vue-webpack-template<br> 2018</b-col>
         <b-col>Contacts</b-col>
