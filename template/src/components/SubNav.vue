82158d3df603004ffac89a82b90dd11796880438
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-05-01 23:59:10 +0300

    Change navbars

--- template/src/components/SubNav.vue
+++ template/src/components/SubNav.vue
@@ -3,7 +3,6 @@
     <b-nav-item active>Active</b-nav-item>
     <b-nav-item>Link</b-nav-item>
     <b-nav-item>Another Link</b-nav-item>
-    <b-nav-item disabled>Disabled</b-nav-item>
   </b-nav>
 </template>
 
