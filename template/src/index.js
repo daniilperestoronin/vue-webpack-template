e94d7504fafb787e57ad7a2fe816dd147dc1ed5f
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-05-11 23:42:54 +0300

    Add routes

--- template/src/index.js
+++ template/src/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -6,10 +7,28 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'font-awesome/css/font-awesome.css';
 
 import App from './App.vue'
+import CardContainer from './components/CardContainer.vue'
 
 Vue.use(BootstrapVue);
 
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'app',
+      component: App
+    },
+    {
+      path: '/CardContainer',
+      name: 'CardContainer',
+      component: CardContainer
+    }
+  ]
+})
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  template: '<App/>',
+  components: { App } 
 })
