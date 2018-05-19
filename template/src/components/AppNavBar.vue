687a7c81bf9d8e1ed8a0c5bcbe0d8e68052ede95
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-05-19 16:33:31 +0300

    Add vue+bootstrap b-jumbotron

--- template/src/components/AppNavBar.vue
+++ template/src/components/AppNavBar.vue
@@ -1,33 +1,25 @@
 <template>
   <b-navbar toggleable="md" type="dark" variant="info">
-
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-  <b-navbar-brand href="#">vue-webpack-template</b-navbar-brand>
-
-  <b-collapse is-nav id="nav_collapse">
-
-    <b-navbar-nav>
-      <b-nav-item href="#/">Home</b-nav-item>
-      <b-nav-item href="#/technologies">Technologies</b-nav-item>
-    </b-navbar-nav>
-
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
-
-      <b-nav-form>
-        <b-nav-item href="#">
-          <i class="fa fa-search"></i>
-          Search
-        </b-nav-item>
-      </b-nav-form>
-      <b-nav-form>
-        <b-nav-item href="#">Sing in</b-nav-item>
-      </b-nav-form>
-
-    </b-navbar-nav>
-  </b-collapse>
-</b-navbar>
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">vue-webpack-template</b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#/">Home</b-nav-item>
+        <b-nav-item href="#/technologies">Technologies</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-nav-item href="#">
+            <i class="fa fa-search"></i>
+            Search
+          </b-nav-item>
+        </b-nav-form>
+        <b-nav-form>
+          <b-nav-item href="#">Sing in</b-nav-item>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
--- template/src/components/technologies/TechnologiesAbout.vue
+++ template/src/components/technologies/TechnologiesAbout.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container class="mt-5" sm="9">
-        <b-jumbotron>
+        <b-jumbotron  bg-variant="light">
             <template slot="header">
                 <b-img width="100" height="100" src="https://ru.vuejs.org/images/logo.png" fluid alt="Fluid image" />
                 Vue
@@ -9,20 +9,42 @@
                 The Progressive JavaScript Framework 
             </template>
             <hr class="my-4">
-            <p>               
-                Approachable
-
-                Already know HTML, CSS and JavaScript? Read the guide and start building things in no time!
-
-                Versatile
-
-                An incrementally adoptable ecosystem that scales between a library and a full-featured framework.
-
-                Performant
-
-                20KB min+gzip Runtime Blazing Fast Virtual DOM Minimal Optimization Efforts
+                <ul>
+                    <li>
+                        Approachable
+                        <br>
+                        Already know HTML, CSS and JavaScript? Read the guide and start building things in no time!
+                    </li>
+                    <li>
+                        Versatile
+                        <br>
+                        An incrementally adoptable ecosystem that scales between a library and a full-featured framework.
+                    </li>
+                    <li>
+                        Performant
+                        <br>
+                        20KB min+gzip Runtime Blazing Fast Virtual DOM Minimal Optimization Efforts
+                    </li>
+                </ul>
+            <b-btn variant="info" href="#">More info</b-btn>
+        </b-jumbotron>
+         <b-jumbotron  bg-variant="light">
+            <template slot="header">
+                <b-img width="100" height="100" 
+                src="https://bootstrap-vue.js.org/_nuxt/img/icon.f0a8c9e.png" fluid alt="Fluid image" />
+                Bootstrap + Vue
+            </template>
+            <template slot="lead">
+                Build responsive, mobile-first projects on the web using Vue.js and the world's most
+                 popular front-end CSS library — Bootstrap V4.
+            </template>
+            <hr class="my-4">
+            <p>
+                Bootstrap-Vue provides one of the most comprehensive implementations of Bootstrap V4
+                components and grid system available for Vue.js 2.4+, complete with extensive and
+                automated WAI-ARIA accessibility markup.
             </p>
-            <b-btn variant="primary" href="#">More info</b-btn>
+            <b-btn variant="info" href="#">More info</b-btn>
         </b-jumbotron>
     </b-container>
 </template>
