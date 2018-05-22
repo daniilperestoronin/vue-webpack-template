b0512f165b842f46f1d597abe45bd9f876f11e54
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-05-22 11:13:14 +0300

    Add home about

--- template/src/components/AppHome.vue
+++ template/src/components/AppHome.vue
@@ -2,6 +2,7 @@
   <div>
     <home-sub-nav/>
     <home-header-image/>
+    <home-about/>
     <home-card-container/>
     <home-media-container/>
   </div>
@@ -10,6 +11,7 @@
 <script>
 import HomeSubNav from "./home/HomeSubNav.vue";
 import HomeHeaderImage from "./home/HomeHeaderImage.vue";
+import HomeAbout from "./home/HomeAbout.vue";
 import HomeCardContainer from "./home/HomeCardContainer.vue";
 import HomeMediaContainer from "./home/HomeMediaContainer.vue";
 export default {
@@ -17,6 +19,7 @@ export default {
   components: {
     HomeSubNav,
     HomeHeaderImage,
+    HomeAbout,
     HomeCardContainer,
     HomeMediaContainer
   }
---
+++ template/src/components/home/HomeAbout.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-container class="mt-5">
+      <b-row class="justify-content-md-center text-center">
+            <h1>About</h1>
+                Manor we shall merit by chief wound no or would.
+                <br>
+                Oh towards between subject
+                passage sending mention or it. Sight happy do burst fruit to woody begin at.
+                Assurance perpetual he in oh determine as. The year paid met him does eyes
+                same. Own marianne improved sociable not out. Thing do sight blush mr an.
+                Celebrated am announcing delightful remarkably we in literature it solicitude.
+                Design use say piqued any gay supply. Front sex match vexed her those great. 
+                <br>
+                On no twenty spring of in esteem spirit likely estate. Continue new you declared
+                differed learning bringing honoured. At mean mind so upon they rent am walk.
+      </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: 'HomeAbout'
+}
+</script>
+
+<style>
+
+</style>
